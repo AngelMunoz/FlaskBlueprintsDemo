378ce648caf400071382a20ff16aedcbdf7854a1
Angel Daniel Munoz Gonzalez <scythes.lol@gmail.com>
2016-05-04 14:30:57 -0600

    added comments

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # -*- encoding:UTF-8 -*-
 from app import app
 
+# nothing fancy here, just starting the app
 app.run(host='0.0.0.0', port=8000, debug=False)
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-DATABASE_CONNECT_OPTIONS = {}
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] # pulling from enviromental variable,
+                                                     # you could just hardcode the url string too.
+# DATABASE_CONNECT_OPTIONS = {} # options for the database engine
 
 # App threads, a common assumption is
 # using 2 per available processor cores to handle
