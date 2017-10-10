c461281bc57ce367df7c1815d639463cc6ac3e3b
Angel D. Munoz <scythes.lol@gmail.com>
2017-10-10 18:03:42 -0500

    Fixed Register Form

--- app/static/js/auth/login.js
+++ app/static/js/auth/login.js
@@ -1,12 +1,11 @@
-/// <reference path="../../../../typings/main.d.ts" />
-$(document).ready(function(){
+$(document).ready(function() {
     var loginForm = $('#loginForm');
-    
+
     var validateEmail = function(email) {
-        var regex = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/;   
+        var regex = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/;
         return regex.test(email);
     }
-    var validateForm = function(form){
+    var validateForm = function(form) {
         var errors = [];
         var valids = [];
         form.serializeArray().forEach(function(element) {
@@ -17,16 +16,16 @@ $(document).ready(function(){
                 valids.push(element.name);
             }
         });
-        
+
         if (errors.length > 0) {
-            errors.forEach(function(errorField){
-                $("#"+ errorField).toggleClass('invalid', true);
+            errors.forEach(function(errorField) {
+                $("#" + errorField).toggleClass('invalid', true);
             });
             return false;
         }
         if (valids.length > 0) {
-            valids.forEach(function(validField){
-                $("#"+ validField).toggleClass('invalid', false);
+            valids.forEach(function(validField) {
+                $("#" + validField).toggleClass('invalid', false);
             });
         }
         return true
@@ -46,38 +45,36 @@ $(document).ready(function(){
             $.ajax({
                 data: formData,
                 url: '/auth/login/',
-                contentType:'application/x-www-form-urlencoded;charset=UTF-8',
+                contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
                 type: "POST",
                 success: function(response) {
                     Materialize.toast("Welcome!", 4000);
-                    setTimeout(function(){
+                    setTimeout(function() {
                         window.location.reload();
-                    },750); 
+                    }, 750);
                 },
                 error: function(error) {
                     var errorObj = JSON.parse(error.responseText);
-                    if(error.status === 400)
-                    {
+                    if (error.status === 400) {
                         for (var key in errorObj) {
                             var errors = "";
                             if (errorObj.hasOwnProperty(key)) {
                                 var element = errorObj[key];
-                                element.forEach(function(err){
+                                element.forEach(function(err) {
                                     errors += err + "\n";
                                 });
-                                Materialize.toast("Found errors in: " + key.toUpperCase() + " Error: " +errors, 5000);
+                                Materialize.toast("Found errors in: " + key.toUpperCase() + " Error: " + errors, 5000);
                             }
                         }
                     }
-                    else if (error.status === 404 || error.status === 409)
-                    {
+                    else if (error.status === 404 || error.status === 409) {
                         for (var key in errorObj) {
                             if (errorObj.hasOwnProperty(key)) {
-                                Materialize.toast("Found: " + key.toUpperCase() + " " + errorObj[key] , 5000);
+                                Materialize.toast("Found: " + key.toUpperCase() + " " + errorObj[key], 5000);
                             }
                         }
                     }
-                    
+
                 }
             });
         }
--- app/static/js/auth/register.js
+++ app/static/js/auth/register.js
@@ -1,33 +1,32 @@
-/// <reference path="../../../../typings/main.d.ts" />
 $(document).ready(function() {
-    
+
     var signupForm = $('#signupForm');
-    
+
     var validateEmail = function(email) {
-        var regex = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/;   
+        var regex = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/;
         return regex.test(email);
     }
-    
-    var validatePassword = function (password){
-      var regex = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/
-      return regex.test(password);  
+
+    var validatePassword = function(password) {
+        var regex = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/
+        return regex.test(password);
     }
-    
-    var validateForm = function(form){
+
+    var validateForm = function(form) {
         var errors = [];
         var valids = [];
-        form.serializeArray().forEach(function(element) {
-            
+        form.forEach(function(element) {
+
             if (element.name === 'email') {
-                if(!validateEmail(element.value) || element.value.length < 1){
+                if (!validateEmail(element.value) || element.value.length < 1) {
                     errors.push(element.name);
                 }
                 else {
                     valids.push(element.name);
                 }
             }
-            if(element.name === 'password') {
-                if(!validatePassword(element.value)  || element.value.length < 1){
+            if (element.name === 'password') {
+                if (!validatePassword(element.value) || element.value.length < 1) {
                     errors.push(element.name);
                 }
                 else {
@@ -49,24 +48,25 @@ $(document).ready(function() {
                 }
             }
         });
-        
+
         if (errors.length > 0) {
-            errors.forEach(function(errorField){
-                $("#"+ errorField).toggleClass('invalid', true);
+            errors.forEach(function(errorField) {
+                $("#" + errorField).toggleClass('invalid', true);
             });
             return false;
         }
         if (valids.length > 0) {
-            valids.forEach(function(validField){
-                $("#"+ validField).toggleClass('invalid', false);
+            valids.forEach(function(validField) {
+                $("#" + validField).toggleClass('invalid', false);
             });
         }
         return true
     }
-    
+
     signupForm.on('submit', function(e) {
         e.preventDefault()
         var formData = $(this).serialize();
+        var fields = $(this).serializeArray();
         var csrftoken = $('meta[name=csrf-token]').attr('content');
         $.ajaxSetup({
             beforeSend: function(xhr, settings) {
@@ -75,17 +75,17 @@ $(document).ready(function() {
                 }
             },
         });
-        if (validateForm($(this))) {
+        if (validateForm(fields)) {
             $.ajax({
                 data: formData,
                 url: '/auth/signup/',
-                contentType:'application/x-www-form-urlencoded;charset=UTF-8',
+                contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
                 type: "POST",
                 success: function(response) {
                     Materialize.toast("Signed Up Succesfully!\n Please Log in!", 4000);
                     setTimeout(function() {
-                        window.location.assign("/auth/login/");    
-                    }, 100);
+                        window.location.assign("/auth/login/");
+                    }, 45000);
                 },
                 error: function(error) {
                     var errorObj = JSON.parse(error.responseText);
@@ -93,16 +93,16 @@ $(document).ready(function() {
                         var errors = "";
                         if (errorObj.hasOwnProperty(key)) {
                             var element = errorObj[key];
-                            element.forEach(function(err){
+                            element.forEach(function(err) {
                                 errors += err + "\n";
                             });
-                            Materialize.toast("Found errors in: " + key.toUpperCase() + " Error: " +errors, 5000);
+                            Materialize.toast("Found errors in: " + key.toUpperCase() + " Error: " + errors, 5000);
                         }
                     }
-                    
+
                 }
-            });   
+            });
         }
-        
+
     });
 });
